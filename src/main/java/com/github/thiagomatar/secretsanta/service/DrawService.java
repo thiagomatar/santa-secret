15f8b5e98e5d0e921edfd783b0249ea87dc3a16f
thiago <thiago.matar@msn.com>
2019-12-03 09:53:44 -0300

    Adding DrawService

--- src/main/java/com/github/thiagomatar/secretsanta/service/DrawService.java
+++ src/main/java/com/github/thiagomatar/secretsanta/service/DrawService.java
@@ -1,10 +1,15 @@
 package com.github.thiagomatar.secretsanta.service;
 
+import com.github.thiagomatar.secretsanta.domain.Draw;
+import com.github.thiagomatar.secretsanta.domain.Group;
 import com.github.thiagomatar.secretsanta.domain.Person;
+import com.github.thiagomatar.secretsanta.repository.DrawRepository;
+import com.github.thiagomatar.secretsanta.repository.GroupRepository;
 import com.github.thiagomatar.secretsanta.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,15 +20,17 @@ public class DrawService {
 
     private final PickerService pickerService;
     private final PersonRepository personRepository;
+    private final GroupRepository groupRepository;
+    private final DrawRepository drawRepository;
 
     public void draw(Long groupId){
-        List<Person> people = this.personRepository.findAllByGroup_Id(groupId);
+        Group group = groupRepository.findById(groupId).orElseThrow(RuntimeException::new);
+        List<Person> people = this.personRepository.findAllByGroup_Id(group.getId());
         Map<Person, Person> pick = this.pickerService.pick(people);
-        pick.forEach((p1, p2) -> {
-            
-        });
 
-
+        List<Draw> draw = new ArrayList<>();
+        pick.forEach((source, target) -> draw.add(Draw.builder().group(group).source(source).target(target).build()));
+        drawRepository.saveAll(draw);
     }
 
 }
